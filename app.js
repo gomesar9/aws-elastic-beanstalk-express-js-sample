9c1a75486074b9ea4fd94043699119e35abd2dd3
gomesar <gomes.ar@pm.me>
2020-10-19 23:15:34 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Here we go'));
+app.get('/', (req, res) => res.send('Full pipeline test. Here we go(2)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
